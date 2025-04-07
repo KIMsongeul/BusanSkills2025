4fff44cf2bec14a41df24e041c618e5bedc1f998
KIMsongeul <0754minjun@gmail.com>
2025-04-07 19:33:28 +0900

    함정 제작 Feat : Trap

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public void SetDamage(int damage)
+    {
+
+    }
+}
--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+
+    public float attackTimer = 5f;
+    public float attackTiming = 0f;
+    public bool isAttack;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player").transform;
+        Vector3 playerPos = player.position;
+        playerPos.y = transform.position.y;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isAttack)
+        {
+            attackTiming += Time.deltaTime;
+            if(attackTiming > attackTimer)
+            {
+                isAttack = false;
+                attackTiming = 0f;
+            }
+        }
+        else
+        {
+            if(Vector3.Distance(transform.position, player.position) < 0.4f)
+            {
+                if (!isAttack)
+                {
+                    isAttack = true;
+                    player.GetComponent<PlayerCtrl>().SetDamage(5);
+                }
+            }
+        }
     }
+
 }
